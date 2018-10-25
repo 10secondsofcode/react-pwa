1ac1636a845da5e75aa023081da38ce7396ebc2b
elangov1 <elangov1@gmail.com>
2018-10-25 16:47:39 +0530

    Initial COmmit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import logo from "./logo.svg";
+
+import Home from "./components/Home";
+import About from "./components/About";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>React App</h2>
+        </div>
+        <BrowserRouter>
+          <div>
+            <Route path="/" exact component={Home} />
+            <Route path="/about" exact component={About} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+export default App;
