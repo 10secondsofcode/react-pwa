0ed2fc6da3ba3a4115fe9815c3123b40903474f5
elangov1 <elangov1@gmail.com>
2018-10-25 18:18:15 +0530

    Updates

---
+++ precache-manifest.b2834d68f7c5c707baabce2a6c44c540.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-pwa/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "bf64366b40f2be799923",
+    "url": "/react-pwa/static/js/runtime~main.bf64366b.js"
+  },
+  {
+    "revision": "d9255bd933245ae84eb8",
+    "url": "/react-pwa/static/js/main.d9255bd9.chunk.js"
+  },
+  {
+    "revision": "17309b219e9b7efe3b08",
+    "url": "/react-pwa/static/js/1.17309b21.chunk.js"
+  },
+  {
+    "revision": "d02779fd6b9388177b8c71ea57c9fa44",
+    "url": "/react-pwa/index.html"
+  }
+];
